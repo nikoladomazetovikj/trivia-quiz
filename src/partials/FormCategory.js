abf20fb0d43eab31a88d7072b9b9bc5ccb151651
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-10 22:23:16 +0200

    wip: difficulty list

--- src/partials/FormCategory.js
+++ src/partials/FormCategory.js
@@ -1,19 +1,24 @@
 import {Col, Form, Row} from "react-bootstrap";
 import CategoryList from "../utilities/CategoryList";
 import QuestionNumberList from "../utilities/QuestionNumberList";
+import DifficultyList from "../utilities/DifficultyList";
 
 const FormCategory = ({props}) => {
     return  <Form>
         <Form.Group as={Col} controlId="formGridState">
             <Form.Label>Categories</Form.Label>
             <Form.Select defaultValue="Choose...">
-                <option key='default'>Choose...</option>
+                <option key='anyCategory'>Choose...</option>
                 <CategoryList/>
             </Form.Select>
             <Form.Label  className='my-3'>Questions</Form.Label>
             <Form.Select defaultValue="Choose...">
-                <option key='defaultq'>Choose...</option>
                 <QuestionNumberList/>
+            </Form.Select>
+            <Form.Label  className='my-3'>Difficulty</Form.Label>
+            <Form.Select defaultValue="Choose...">
+                <option key='anyDifficulty'>Choose...</option>
+                <DifficultyList/>
             </Form.Select>
         </Form.Group>
     </Form>
---
+++ src/utilities/DifficultyList.js
@@ -0,0 +1,13 @@
+
+const DifficultyList = () => {
+    return (
+        <>
+            <option key='easy' value='easy'>Easy</option>
+            <option key='medium' value='medium'>Medium</option>
+            <option key='hard' value='10'>Hard</option>
+        </>
+    );
+};
+
+
+export default DifficultyList;
