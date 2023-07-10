8f8cb6b367390b77ed5ea1ccc57b0759907a9be4
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-10 22:38:08 +0200

    fix: get selected values

--- src/partials/FormGenerate.js
+++ src/partials/FormGenerate.js
@@ -3,20 +3,35 @@ import CategoryList from "../utilities/CategoryList";
 import QuestionNumberList from "../utilities/QuestionNumberList";
 import DifficultyList from "../utilities/DifficultyList";
 
-const FormGenerate = ({props}) => {
+const FormGenerate = ({onSelectCategory,onSelectDifficulty,onSelectQuestion}) => {
+    const handleCategoryChange = (event) => {
+        const selectedCategory = event.target.value;
+        onSelectCategory(selectedCategory);
+    };
+
+    const handleQuestionChange = (event) => {
+        const selectedQuestion = event.target.value;
+        onSelectQuestion(selectedQuestion);
+    };
+
+    const handleDifficultyChange = (event) => {
+        const selectedDifficulty = event.target.value;
+        onSelectDifficulty(selectedDifficulty);
+    };
+
     return  <Form>
         <Form.Group as={Col} controlId="formGridState">
             <Form.Label>Categories</Form.Label>
-            <Form.Select defaultValue="Choose...">
+            <Form.Select defaultValue="Choose..." onChange={handleCategoryChange}>
                 <option key='anyCategory'>Choose...</option>
                 <CategoryList/>
             </Form.Select>
             <Form.Label  className='my-3'>Questions</Form.Label>
-            <Form.Select defaultValue="Choose...">
+            <Form.Select defaultValue="Choose..." onChange={handleQuestionChange}>
                 <QuestionNumberList/>
             </Form.Select>
             <Form.Label  className='my-3'>Difficulty</Form.Label>
-            <Form.Select defaultValue="Choose...">
+            <Form.Select defaultValue="Choose..." onChange={handleDifficultyChange}>
                 <option key='anyDifficulty'>Choose...</option>
                 <DifficultyList/>
             </Form.Select>
