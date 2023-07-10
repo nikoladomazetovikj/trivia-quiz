7f958895e68114da9bfcc93f6fdf434645339573
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-10 23:08:56 +0200

    fix: nesting dom

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,19 +1,21 @@
 import {Button, Card, Container, Row} from "react-bootstrap";
 
 const Selection = ({children, buttonState}) => {
-    return <Container>
-        <Row>
+    return (
+        <Container>
             <Card>
-                <Card.Header as="h5" className='text-center'>Select Questions</Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        {children}
-                    </Card.Text>
-                    <Button variant="primary" disabled={buttonState}>Start</Button>
-                </Card.Body>
+                <Card.Header as="h5" className="text-center">
+                    Select Questions
+                </Card.Header>
+                <Card.Body>{children}</Card.Body>
+                <Card.Footer>
+                    <Button variant="primary" disabled={buttonState}>
+                        Start
+                    </Button>
+                </Card.Footer>
             </Card>
-        </Row>
-    </Container>
+        </Container>
+    );
 };
 
 export default Selection;
