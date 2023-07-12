9bae9397d406ef9873bc0ac38a88af96e70a9768
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-12 20:03:33 +0200

    fix: typo

--- src/utilities/DifficultyList.js
+++ src/utilities/DifficultyList.js
@@ -5,7 +5,7 @@ const DifficultyList = () => {
             <option key='any' value='any'>Any</option>
             <option key='easy' value='easy'>Easy</option>
             <option key='medium' value='medium'>Medium</option>
-            <option key='hard' value='10'>Hard</option>
+            <option key='hard' value='hard'>Hard</option>
         </>
     );
 };
