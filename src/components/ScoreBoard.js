1186892eeb2b464fe48184c49bcce4e7bfc0dbb7
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-12 19:49:03 +0200

    fix: handle different design of scoreboard according to the results

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,18 +1,28 @@
 import {Alert, Button, Col, Row} from "react-bootstrap";
 
 const ScoreBoard = ({score, totalQuestions}) => {
+
+    let color;
+    if (score < 3) {
+        color = 'danger';
+    } else if (score >= 3 && score < 7) {
+        color = 'warning';
+    } else if (score >= 7) {
+        color = 'success';
+    }
+
     return (
         <>
             <Row>
                 <Col className='text-center'>
-                    <Alert  variant="success">
+                    <Alert  variant={color}>
                         <Alert.Heading>Your Score</Alert.Heading>
                         <p>
                             {score} / {totalQuestions}
                         </p>
                         <hr />
                         <div className="d-flex justify-content-end">
-                            <Button  variant="outline-success">
+                            <Button variant={"outline-" + color}>
                                 Start Again
                             </Button>
                         </div>
